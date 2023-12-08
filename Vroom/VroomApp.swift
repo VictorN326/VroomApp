b5bac28a5193125d0feb1a47beb518e715185c34
VictorN326 <21vnguyen3@gmail.com>
2023-12-07 18:18:50 -0800

    Initial Commit

---
+++ Vroom/VroomApp.swift
@@ -0,0 +1,17 @@
+//
+//  VroomApp.swift
+//  Vroom
+//
+//  Created by Victor Nguyen on 12/7/23.
+//
+
+import SwiftUI
+
+@main
+struct VroomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
